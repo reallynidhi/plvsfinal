a0777101f9b8934d11adcc613d9e3b513667e034
Nidhi <nidhirajeshsoni1698@gmail.com>
2025-01-14 21:50:20 +0530

    mail icon added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -56,6 +56,24 @@ const Navbar = () => {
         >
           <XLogo />
         </a>
+        <a
+          href="mailto:plvs@protocol.ai"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-gray-600"
+          aria-label="Email"
+        >
+         <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6"
+        >
+          <path d="M4 4h16a2 2 0 012 2v12a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2zm16 2H4v.01L12 12l8-5.99V6zm-8 7L4 7.01V18h16V7.01L12 13z" />
+        </svg>
+
+        </a>
+
       </div>
     </nav>
   );
