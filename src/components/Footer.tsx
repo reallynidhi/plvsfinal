dd0dad4809f87765648dc10dcd5a8386a519684c
Nidhi <nidhirajeshsoni1698@gmail.com>
2025-01-20 21:41:02 +0530

    fixed height

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="mt-20 py-8 px-8 md:px-24 border-t">
-      <div className="flex flex-col justify-between  items-start">
-        <div className="w-12 mb-10">
+    <footer className="mt-20 py-8 px-8 md:px-24 border-t bg-white-800 text-black h-40 flex items-center justify-between">
+      <div className="flex flex-col items-start">
+        <div className="w-12 mb-2">
           <Logo />
         </div>
 
